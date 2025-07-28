6ae05b91da43580c2d12bf2377db2629206ba363
ryusseikun <nikhil.patil23@vit.edu.in>
2025-07-28 22:33:52 +0530

    task1,3

---
+++ Task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=a+b
+print("Addition:", c)
+c=a-b
+print("Subtraction:", c)
+c=a*b
+print("Multiplication:",c)
+c=a/b
+print("Division:", c)
---
+++ Task2.py
@@ -0,0 +1,3 @@
+fname=input("Enter the first name:")
+lname=input("Enter the last name:")
+print("Hello "+fname+" "+lname+"! Nice to meet you")
